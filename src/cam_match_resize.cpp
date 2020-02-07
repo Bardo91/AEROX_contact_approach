431c17e04c730420d4d415107f401f35680b81d8
Ana <anacasadofauli@gmail.com>
2020-02-07 10:01:11 +0100

    Template matching finished

--- src/cam_match_resize.cpp
+++ src/cam_match_resize.cpp
@@ -11,21 +11,31 @@
 using namespace std;
 using namespace cv;
 
-void MatchingMethod(Mat _input, Mat _temp);
+void MatchingMethod(Mat &_input, std::vector<cv::Mat>&_temp);
 
 int main(int argc, char **argv) {
+    int i;
+    std::vector<Mat> templ_resize;
     // Declare depth colorizer for pretty visualization of depth data
     rs2::colorizer color_map;
     // Declare RealSense pipeline, encapsulating the actual device and sensors
     rs2::pipeline pipe;
     // Start streaming with default recommended configuration
     pipe.start();
-
-    Mat templ = imread("/home/ana/programming/AEROX_contact_approach/botella4.jpg", CV_LOAD_IMAGE_COLOR);
+    
+    Mat templ = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    cout << argv[1] << endl;
 
     if( templ.rows == 0 ) { // Check for invalid template
         cout <<  "Could not open or find the image" << std::endl ;
         return -1;
+    }
+
+    for (i=1; i<3;i++){
+        cv::Mat scaledTemplate;
+        resize(templ, scaledTemplate, cv::Size(), (1-i*0.25), (1-i*0.25));
+
+        templ_resize.push_back(scaledTemplate);
     }
 
     while (true) {
@@ -41,43 +51,75 @@ int main(int argc, char **argv) {
         //Realsense camera is in BGR so we need to change it to RGB
         cvtColor(img, img, CV_RGB2BGR);
 
-        MatchingMethod(img, templ);
+        MatchingMethod(img, templ_resize);
         waitKey(3);
     }
     return EXIT_SUCCESS;
 }
 
-void MatchingMethod(Mat _input, Mat _temp){
+void MatchingMethod(Mat &_input, std::vector<cv::Mat> &_temp){
+    Mat final_result;
     Mat result;
+    Mat eval;
 
+    int i;
+    int cols, rows;
     double minVal;
+    double minVal_act=800;
     double maxVal;
+    double maxVal_act=0;
     Point minLoc;
     Point maxLoc;
     Point matchLoc;
 
     const char *image_window = "Source Image";
     const char *result_window = "Result window";
-    int match_method=TM_SQDIFF_NORMED;
+
+    //uncomment matching method preferred
+    //int match_method=TM_SQDIFF_NORMED;
+    //int match_method=TM_CCORR;
+    //int match_method=TM_CCOEFF;
+    int match_method=TM_CCOEFF_NORMED;
 
     Mat img_display;
     _input.copyTo(img_display);
-    int result_cols = _input.cols - _temp.cols + 1;
-    int result_rows = _input.rows - _temp.rows + 1;
-    result.create(result_rows, result_cols, CV_32FC1);
 
-    //function where the template is matched with the camera image
-    matchTemplate(_input, _temp, result, match_method);
-    normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+    for(i=0;i<_temp.size();i++){
+        eval=_temp[i];
+        int result_cols = _input.cols - eval.cols + 1;
+        int result_rows = _input.rows - eval.rows + 1;
+        result.create(result_rows, result_cols, CV_32FC1);
 
-    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-    matchLoc = minLoc; //we use this because it corresponds to the method I chose
+        //function where the template is matched with the camera image
+        matchTemplate(_input, eval, result, match_method);
+        normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
 
+        minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
+        //only TM_SQDIFF_NORMED method uses min values. 
+        if(match_method==TM_SQDIFF_NORMED){
+            if (minVal<minVal_act) {
+                minVal_act=minVal;
+                matchLoc = minLoc; 
+                cols=eval.cols;
+                rows=eval.rows;
+            }
+        }
+        else{
+            if (maxVal>maxVal_act) {
+                maxVal_act=maxVal;
+                matchLoc = maxLoc; 
+                cols=eval.cols;
+                rows=eval.rows;
+            }
+        }
+        
+    }
     /*We have two images displayed: the actual camera image and a gray scale 
     image(result) that represents the amount of matching made by the template 
     in each point (more white=more matching)*/
-    rectangle(img_display, matchLoc, Point(matchLoc.x + _temp.cols, matchLoc.y + _temp.rows), Scalar::all(0), 2, 8, 0);
-    rectangle(result, matchLoc, Point(matchLoc.x + _temp.cols, matchLoc.y + _temp.rows), Scalar::all(0), 2, 8, 0);
+    rectangle(img_display, matchLoc, Point(matchLoc.x + cols, matchLoc.y + rows), Scalar::all(0), 2, 8, 0);
+    rectangle(result, matchLoc, Point(matchLoc.x + cols, matchLoc.y + rows), Scalar::all(0), 2, 8, 0);
     imshow(image_window, img_display);
     imshow(result_window, result);
     return;
